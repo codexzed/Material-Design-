ae6a716f1c0b01bb66c7a9f765c5fa3466910198
codexzed <44883001+codexzed@users.noreply.github.com>
2018-11-09 02:48:15 +0300

    Add files via upload

---
+++ material-design-login-form-with-php-and-jquery/members.sql
@@ -0,0 +1,44 @@
+--
+-- Database: `db_auth`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `member_id` int(8) NOT NULL,
+  `member_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `member_password` varchar(64) NOT NULL,
+  `member_email` varchar(255) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`member_id`, `member_name`, `member_password`, `member_email`) VALUES
+(1, 'vincy', '$2a$10$0FHEQ5/cplO3eEKillHvh.y009Wsf4WCKvQHsZntLamTUToIBe.fG', 'user@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`member_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `member_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
